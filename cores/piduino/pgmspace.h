26a6f285f1d40e44ba23ee40b51748822f807822
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2022-02-06 12:07:53 +0100

    pgmspace: __FlashStringHelper as class, add _P functions

--- cores/piduino/pgmspace.h
+++ cores/piduino/pgmspace.h
@@ -1,7 +1,10 @@
 #ifndef PGMSPACE_INCLUDE
 #define PGMSPACE_INCLUDE
 
-typedef char __FlashStringHelper;
+#ifdef __cplusplus
+class __FlashStringHelper;
+#endif
+
 #define PROGMEM
 #define PSTR(s) (s)
 #define F(string_literal) (reinterpret_cast<const __FlashStringHelper *>(PSTR(string_literal)))
@@ -23,8 +26,22 @@ typedef char __FlashStringHelper;
 #define memcpy_P(to,from,len) memcpy(to,from,len)
 
 // https://github.com/esp8266/Arduino/blob/85ba53a24994db5ec2aff3b7adfa05330a637413/tests/host/sys/pgmspace.h
+#define memccpy_P     memccpy
+#define memmem_P      memmem
+#define strcpy_P      strcpy
+#define strncpy_P     strncpy
+#define strcat_P      strcat
+#define strncat_P     strncat
 #define strcmp_P strcmp
 #define strncmp_P strncmp
-#define sprintf_P sprintf
+#define strcasecmp_P  strcasecmp
+#define strncasecmp_P strncasecmp
+#define strlen_P      strlen
+#define strnlen_P     strnlen
+#define strstr_P      strstr
 #define printf_P printf
+#define sprintf_P     sprintf
+#define snprintf_P    snprintf
+#define vsnprintf_P   vsnprintf
+
 #endif
